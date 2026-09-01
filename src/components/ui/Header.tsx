dbf6b406d1662e9411e6d26a4dcaaa5518faf124
jiyeonyooo <ji04230423@gmail.com>
2026-09-01 16:24:50 +0900

    fix: 네비게이션 라우팅 경로 수정

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -9,7 +9,7 @@ import { NavItem } from './NavItem'
 
 const links = [
   { label: 'HOME', path: '/' },
-  { label: 'ROOMS', path: '/reservations' },
+  { label: 'ROOMS', path: '/rooms' },
   { label: 'RESERVATION', path: '/my-reservations' },
   { label: 'PROGRAM', path: '/programs' },
   { label: 'WELLNESS', path: '/wellness' },
